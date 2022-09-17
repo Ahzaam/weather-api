e7796d64547aa17f56a4f1540db7bde5c0f41d15
Ahzaam <99543407+Ahzaam@users.noreply.github.com>
2022-09-17 23:27:28 +0530

    i have no idea what im doing

--- controller/draw.js
+++ controller/draw.js
@@ -1,3 +1,17 @@
+// For building on vercel: https://github.com/Automattic/node-canvas/issues/1779
+if (
+    process.env.LD_LIBRARY_PATH == null ||
+    !process.env.LD_LIBRARY_PATH.includes(
+      `${process.env.PWD}/node_modules/canvas/build/Release:`,
+    )
+  ) {
+    process.env.LD_LIBRARY_PATH = `${
+      process.env.PWD
+    }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+  }
+
+
+
 const { createCanvas, loadImage, registerFont } = require('canvas')
 
 registerFont('./assets/fonts/Quicksand-VariableFont_wght.ttf', { family: 'Lato' ,weight:200})
