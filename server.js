693d23491f0689d8fd76ebeced7dd1dd552e74fc
Ahzaam <99543407+Ahzaam@users.noreply.github.com>
2022-09-17 23:29:36 +0530

    still no idea

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 const express = require("express");
 const app = express();
 
+
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`,
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+}
+
+
+
+
+
+
+
+
+
 const cors = require("cors");
 const crypto = require("crypto");
 require("dotenv").config();
