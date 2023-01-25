069f6e73614ba612ac230513672f46b987b1bc12
Fawmee Ahzam <93484273+dev001-silverlineit@users.noreply.github.com>
2023-01-25 16:51:27 +0530

    changes

--- apis/weather.js
+++ apis/weather.js
@@ -41,8 +41,8 @@ Weather = {
           }
         )
         .on("error", (err) => {
-          console.error(err)
-          reject(err + 'get air data');
+          console.error(err);
+          reject(err + "get air data");
         });
     }),
 
@@ -65,8 +65,8 @@ Weather = {
           }
         )
         .on("error", (err) => {
-          console.error(err)
-          reject(err + 'get air data');
+          console.error(err);
+          reject(err + "get air data");
         });
     }),
 
@@ -79,15 +79,13 @@ Weather = {
         .then(async (result) => {
           all_data.Weather = JSON.parse(result);
           // getting air data by lon, lat
-          all_data.Weather.error = 'check this'
+          all_data.Weather.error = "check this";
 
           await Weather.getAirData(
-            
-            all_data.Weather.coord.longitude,
-            all_data.Weather.coord.latitude
+            all_data.Weather.coord.lon,
+            all_data.Weather.coord.lat
           )
             .then((result) => {
-                
               all_data.Air = JSON.parse(result);
 
               resolve(all_data);
@@ -96,12 +94,12 @@ Weather = {
             .catch((err) => {
               all_data.Air = { message: "Air data Not Found" };
               // returning without air data
-              console.error(err)
+              console.error(err);
               reject(`{"message": ${err}}, "task":"get air data"`);
             });
         })
         .catch((err) => {
-          console.error(err)
+          console.error(err);
           reject(`{"message": ${err}}, "task":"get weather data"`);
         });
     }),
@@ -109,69 +107,65 @@ Weather = {
   getAllDataByIp: (ip) =>
     new Promise(async (resolve, reject) => {
       ip = ip === undefined ? "8.8.8.8" : ip;
-     
-        
+
       await Weather.getIpLocation(ip)
         .then(async (ipresponse) => {
           let ipjsondata = JSON.parse(ipresponse);
           await Weather.getAllDataByCity(ipjsondata.city)
-            
+
             .then((responce) => {
-                responce.Ip = ipjsondata
-                resolve(responce)})
-            .catch((err) =>{
-              console.error(err)
-                reject(`{"message": ${err}}, "task":"get Ip location"`)
-            }
-              
-            );
+              responce.Ip = ipjsondata;
+              resolve(responce);
+            })
+            .catch((err) => {
+              console.error(err);
+              reject(`{"message": ${err}}, "task":"get Ip location"`);
+            });
         })
         .catch((err) => {
-          console.error(err)
+          console.error(err);
           resolve(`{"message": ${err}}, "task":"get all data by ip"`);
         });
     }),
 
-    getIpLocation : (ip) => new Promise((resolve, reject) => {
-        
-            https.get(`${process.env.IPLOCATION_URL}&ip=${ ip }`, (resp) => {
-                let data = ''
-                // A chunk of data has been received.
-                resp.on('data', (chunk) => {
-                  data += chunk;
-                  
-                });
-              
-           
-                // The whole response has been received. Print out the result.
-                resp.on('end', () => {
-                   resolve(data)
-                });
-              
-              }).on("error", (err) => {
-                reject(err + 'get ip location')
-              })
-        
-        }),
+  getIpLocation: (ip) =>
+    new Promise((resolve, reject) => {
+      https
+        .get(`${process.env.IPLOCATION_URL}&ip=${ip}`, (resp) => {
+          let data = "";
+          // A chunk of data has been received.
+          resp.on("data", (chunk) => {
+            data += chunk;
+          });
 
-        getForCastByCity: (city) => new Promise(async (resolve, reject) => {
-          let data = {}
-          await Weather.getWeather(city)
-          .then(async (responce) => {
-            jsonres = JSON.parse(responce)
+          // The whole response has been received. Print out the result.
+          resp.on("end", () => {
+            resolve(data);
+          });
+        })
+        .on("error", (err) => {
+          reject(err + "get ip location");
+        });
+    }),
 
-            data.current = jsonres
-            await Weather.getForcastByCordinates(jsonres.coord.lon, jsonres.coord.lat)
-            .then((responce) => {
-              data.forcast = JSON.parse(responce)
-              resolve(data)
-            })
+  getForCastByCity: (city) =>
+    new Promise(async (resolve, reject) => {
+      let data = {};
+      await Weather.getWeather(city)
+        .then(async (responce) => {
+          jsonres = JSON.parse(responce);
 
-
-          })
-          .catch((err) => reject(err))
-
-        }) 
+          data.current = jsonres;
+          await Weather.getForcastByCordinates(
+            jsonres.coord.lon,
+            jsonres.coord.lat
+          ).then((responce) => {
+            data.forcast = JSON.parse(responce);
+            resolve(data);
+          });
+        })
+        .catch((err) => reject(err));
+    }),
 };
 
 module.exports = Weather;
